dccd077da8b33cdebb158f602f4d4b5ad41b8a0c
Nelle Favoreel <nelle.favoreel@student.ehb.be>
2022-10-19 15:20:53 +0200

    finish ex3-c

---
+++ exercises/3-iteration-while/3-1-c.js
@@ -0,0 +1,23 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawCross();
+}
+
+function drawCross() {
+    let i = 0;
+    while (i < 5) {
+        let step = 150;
+        Utils.drawLine(50, step + (i *25), 350, step + (i * 25));
+        Utils.drawLine(step + (i * 25), 50, step + (i*25), 350);
+        i++;
+    }
+}
