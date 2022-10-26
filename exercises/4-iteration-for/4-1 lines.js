2ee16a22c3f96dc6bfbbbd4b56c62a3df5188977
Nelle Favoreel <nelle.favoreel@student.ehb.be>
2022-10-26 14:12:48 +0200

    finish ex 1

---
+++ exercises/4-iteration-for/4-1 lines.js
@@ -0,0 +1,28 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let horizontaLines = 60;
+let verticaLines = 30;
+
+
+
+let width = 600;
+let height = 300;
+
+drawLines();
+drawMoreLines();
+
+function drawLines() {
+    let step = width / horizontaLines
+    for (let i = 0; i < horizontaLines; i++) {
+        Utils.drawLine(0 +(10 * i),0,width - (step * i),height);
+    }
+}
+
+function drawMoreLines() {
+    let step = height / verticaLines
+    for (let i = 0; i < verticaLines; i++) {
+        Utils.drawLine(0 ,0+(10 *i),width, height- (step * i))
+    }
+}
---
+++ exercises/4-iteration-for/4-2 Random bubbles.js
@@ -0,0 +1,9 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+randomBubbles();
+
+function randomBubbles() {
+    for(let i + 0 <20; i++)
+}
